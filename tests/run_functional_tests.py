71c92b72e3e1ef507e00f82e6c132fe08da375bb
louib <code@louib.net>
2020-07-16 00:46:58 -0400

    Pushing the python test runner.

---
+++ tests/run_functional_tests.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# import os
+
+# SCRIPT_DIR=$(realpath "$0")
+# SCRIPT_DIR=$(dirname "$SCRIPT_DIR")
+
+# FIXTURES_DIR="$SCRIPT_DIR/fixtures/"
+# OUTPUT_DIR="$SCRIPT_DIR/output/"
+
+# Cleaning the output dir.
+# mkdir -p "$OUTPUT_DIR"
+# rm "$OUTPUT_DIR/*"
+
+print("🔍 Starting functional test suite for 2flatpak.")
+# fixtures_files=$(find "$FIXTURES_DIR" | sort)
+
+
+if __name__ == '__main__':
+    ...
