56640ca62ad6267e1b61668d30342003d483398c
louib <code@louib.net>
2020-07-16 01:08:39 -0400

    Finishing migration to python tests.

--- tests/run_functional_tests.py
+++ tests/run_functional_tests.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
-# import os
+from os import listdir, system
+from os.path import isfile, join
 
-# SCRIPT_DIR=$(realpath "$0")
-# SCRIPT_DIR=$(dirname "$SCRIPT_DIR")
 
-# FIXTURES_DIR="$SCRIPT_DIR/fixtures/"
-# OUTPUT_DIR="$SCRIPT_DIR/output/"
+# FIXME make this relative to the current script.
+FIXTURES_DIR = "tests/fixtures"
+OUTPUT_DIR = "tests/output"
 
 # Cleaning the output dir.
-# mkdir -p "$OUTPUT_DIR"
-# rm "$OUTPUT_DIR/*"
+system("mkdir -p {0}".format(OUTPUT_DIR))
+if len(listdir(OUTPUT_DIR)):
+    system("rm {0}/*".format(OUTPUT_DIR))
 
 print("🔍 Starting functional test suite for 2flatpak.")
-# fixtures_files=$(find "$FIXTURES_DIR" | sort)
-
 
 if __name__ == '__main__':
-    ...
+    for fixtures_file in listdir(FIXTURES_DIR):
+
+        # sanity check, we should be dealing with files at that point.
+        if not isfile(join(FIXTURES_DIR, fixtures_file)):
+            continue
+
+        if not fixtures_file.endswith('.yaml'):
+            continue
