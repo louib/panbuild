10a3b5fe57d0c5cc35323a5ebaecbc0f663e3512
louib <code@louib.net>
2020-07-16 00:12:26 -0400

    Basic test template

--- tests/run_functional_tests.sh
+++ tests/run_functional_tests.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
 FIXTURES_DIRS="./fixtures/"
+
+echo "🔍 Starting functional test suite for 2flatpak."
+fixtures_files=$(find "$FIXTURES_DIR")
+
+IFS=$'\n'; for file in $fixtures_files; do
+    if [[ -d "$file" ]]; then
+        continue
+    fi
+
+    # Sanity check. At that point we should really be dealing with valid filenames.
+    if [[ ! -f "$file" ]]; then
+        die "$file is not a file!"
+    fi
+
+    echo "$file"
+done
