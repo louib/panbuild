eaadc85044b5222dd4816552403c2ea8bc22c209
louib <code@louib.net>
2020-07-16 00:46:12 -0400

    Migrating to a python file for the tests.

--- tests/run_functional_tests.sh
+++ tests/run_functional_tests.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-FIXTURES_DIR="./fixtures/"
+SCRIPT_DIR=$(realpath "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_DIR")
+
+FIXTURES_DIR="$SCRIPT_DIR/fixtures/"
+OUTPUT_DIR="$SCRIPT_DIR/output/"
+
+# Cleaning the output dir.
+mkdir -p "$OUTPUT_DIR"
+rm "$OUTPUT_DIR/*"
 
 echo "🔍 Starting functional test suite for 2flatpak."
-fixtures_files=$(find "$FIXTURES_DIR")
+fixtures_files=$(find "$FIXTURES_DIR" | sort)
 
 IFS=$'\n'; for file in $fixtures_files; do
     if [[ -d "$file" ]]; then
@@ -15,5 +23,11 @@ IFS=$'\n'; for file in $fixtures_files; do
         die "$file is not a file!"
     fi
 
-    echo "$file"
+    test_file_name=
+
+    output_file_path="$OUTPUT_DIR"
+    if [[ "$file" =~ .*\.yml$ ]]; then 
+        echo "$file is a yaml file!!"
+        coverage run "$SCRIPT_DIR/../src/snap2flatpak" "$file" > "lol.yaml"
+    fi
 done
