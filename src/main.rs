e9737f7856f45f0d0cd5cf23a7ff5ef8931e4438
louib <code@louib.net>
2020-07-17 16:42:20 -0400

    Importing manifest.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate yaml_rust;
 // use yaml_rust::{YamlLoader, YamlEmitter};
 use yaml_rust::{YamlLoader};
 
+mod manifest;
+
 fn main() {
     let s = "---";
     YamlLoader::load_from_str(&s).unwrap();
--- src/manifest.rs
+++ src/manifest.rs
@@ -1,6 +1,5 @@
 // Base interface for a build manifest.
 trait Manifest {
 
-    fsdfsdfsdfsdfn is_valid(&self);
     fn to_string(&self);
 }
