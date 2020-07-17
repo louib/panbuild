ab0267c589671df3252ffc4626c8be0b18835009
louib <code@louib.net>
2020-07-17 16:40:41 -0400

    Adding basic manifest.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate yaml_rust;
 
-use yaml_rust::{YamlLoader, YamlEmitter};
+// use yaml_rust::{YamlLoader, YamlEmitter};
+use yaml_rust::{YamlLoader};
 
 fn main() {
-    let source_manifest = YamlLoader::load_from_str(s).unwrap();
+    let s = "---";
+    YamlLoader::load_from_str(&s).unwrap();
 
     println!("Hello, world!");
 }
---
+++ src/manifest.rs
@@ -0,0 +1,6 @@
+// Base interface for a build manifest.
+trait Manifest {
+
+    fsdfsdfsdfsdfn is_valid(&self);
+    fn to_string(&self);
+}
