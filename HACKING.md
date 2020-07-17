ff5073fff704aef6e61ca832f0414782787918cd
louib <code@louib.net>
2020-07-17 19:00:13 -0400

    path_is_manifest_type

---
+++ HACKING.md
@@ -0,0 +1,6 @@
+# Hack the planet!!
+
+Make sure to run `rustfmt` on your code before commiting:
+```
+rustup component add rustfmt
+```
--- src/manifest.rs
+++ src/manifest.rs
@@ -1,5 +1,9 @@
 // Base interface for a build manifest.
 trait Manifest {
 
-    fn to_string(&self);
+    fn to_string(&self) -> String;
+
+    // This function detects if a file path could be a potential candidate
+    // for this build system.
+    fn path_is_manifest_type(&self);
 }
