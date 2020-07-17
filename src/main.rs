d3c3d2d899284a668bf169c7d5972485983b4900
louib <code@louib.net>
2020-07-17 19:59:09 -0400

    Almost there I guess.

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ extern crate clap;
 
 // use yaml_rust::{YamlLoader, YamlEmitter};
 use yaml_rust::{YamlLoader};
-use clap::{Arg, App};
+use clap::{Arg, App, ArgMatches};
 use std::fs;
 use std::path;
+use std::process::{exit};
 
 mod manifest;
 mod snap;
@@ -13,18 +14,30 @@ mod flatpak;
 mod pyproject;
 
 fn main() {
-    let matches = App::new("panbuild")
+    let matches: ArgMatches = App::new("panbuild")
                           .version("0.0.1")
                           .author("louib <code@louib.net>")
                           .about("The universal build manifest converter.")
                           .arg(Arg::with_name("input_file")
                                .short("i")
                                .long("input-file")
+                               .takes_value(true)
                                .value_name("MANIFEST")
                                .required(false)
-                               .help("Path of the input build manifest.")
-                               .takes_value(true))
+                               .help("Path of the input build manifest."))
+                          .arg(Arg::with_name("version")
+                               .short("V")
+                               .long("version")
+                               .required(false)
+                               .help("Show the version and exit."))
                           .get_matches();
+
+    if matches.is_present("version") {
+        println!("0.0.1");
+        exit(0);
+    }
+    exit(0);
+
 
     let input_file = matches.value_of("input_file").unwrap();
     let input_file_path = path::Path::new(input_file);
