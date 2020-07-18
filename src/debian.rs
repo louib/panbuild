3da99ce6a6c2ee77be32b69b9d79cfd3b44ce7ea
louib <code@louib.net>
2020-07-17 23:54:56 -0400

    Basic debian control tags

--- src/debian.rs
+++ src/debian.rs
@@ -1,10 +1,34 @@
 struct DebianManifest {
+    // The name of the source described in this manifest.
     source: String,
+
+    // Can be:
+    //   * libs
     section: String,
+
+    // Can be:
+    //   * optional
     priority: String,
 
     // Format is name <email@address.com>
     maintainer: String,
 
     build_depends: Vec<String>,
+
+    // A semver reference to a "canonical" version.
+    standards_versions: String,
+
+    homepage: String,
+
+    // URL of a website to browser the source code.
+    vcs_browser: String,
+
+    // URL of the git repo.
+    vcs_git: String,
+
+    packages: Vec<Package>,
+}
+
+struct Package {
+
 }
