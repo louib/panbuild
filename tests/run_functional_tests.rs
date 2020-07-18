9e47cfa193fcacf0264c629665f0f0eee91638c5
louib <code@louib.net>
2020-07-17 23:32:45 -0400

    Functional tests are compiling.

--- tests/run_functional_tests.rs
+++ tests/run_functional_tests.rs
@@ -1,20 +1,23 @@
 use std::process::Command;
 
 // FIXME make this relative to the current script.
-static FIXTURES_DIR: String = "tests/fixtures";
-static OUTPUT_DIR: String = "tests/output";
-static EXPECTED_DIR: String = "tests/expected";
+static FIXTURES_DIR: &str = "tests/fixtures";
+static OUTPUT_DIR: &str = "tests/output";
+static EXPECTED_DIR: &str = "tests/expected";
 
 fn cleanup_output_dir() {
-    // mkdir -p "$OUTPUT_DIR"
+    Command::new("mkdir")
+        .arg(format!("-p {}", OUTPUT_DIR))
+        .output()
+        .expect("");
     // rm "{0}/*"
 }
 
 fn main() {
     Command::new("command")
-        .args("-v panbuild")
+        .arg("-v panbuild")
         .output()
         .expect("");
 
-    println!("🔍 Starting functional test suite for panbuild.");
+    println!("🔍 Starting functional test suite for panbuild. 🔍");
 }
