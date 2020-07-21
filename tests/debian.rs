307f51e3eeb0ed0e90a2088868259eae378155e9
louib <code@louib.net>
2020-07-21 17:49:38 -0400

    test template.

--- tests/debian.rs
+++ tests/debian.rs
@@ -1,6 +1,13 @@
 // use panbuild::manifests::debian::{parse, dump};
+#[cfg(test)]
+mod debian_tests {
+    #[test]
+    pub fn test_parse() {
 
-#[test]
-pub fn test_parse() {
+    }
 
+    #[test]
+    pub fn test_dump() {
+
+    }
 }
