af0c54702cae6f657abb9b2a19ad2ab624cdd93a
louib <code@louib.net>
2020-07-29 00:05:39 -0400

    Interactive sed

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -53,7 +53,7 @@ main_file_path="$SCRIPT_DIR/../src/main.rs"
 if [[ ! -f "$main_file_path" ]]; then
     die "Could not find main file $main_file_path";
 fi
-sed "s/const APP_VERSION: &str = \"$current_version\";/const APP_VERSION: &str = \"$new_version\";/g" "$main_file_path"
+sed -i "s/const APP_VERSION: &str = \"$current_version\";/const APP_VERSION: &str = \"$new_version\";/g" "$main_file_path"
 
 # git commit -a -n -m "🏷️ $new_version 🏷️"
 
