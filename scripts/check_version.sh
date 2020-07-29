b8118e64feb08b33a0ddebfc15bce9089ce22a7e
louib <code@louib.net>
2020-07-28 20:49:05 -0400

    Add check_version script.

---
+++ scripts/check_version.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Check that the multiple instances of the app version are in sync.
+
+die() { echo "🔥 Error: $*" 1>&2; exit 1; }
+
+SCRIPT_DIR=$(dirname "$0")
+
+
+version_file_path="$SCRIPT_DIR/../VERSION"
+if [[ ! -f "$version_file_path" ]]; then
+    die "Could not find version file $version_file_path";
+fi
+app_version=$(cat "$version_file_path")
+
+echo "Verifying that version is $app_version everywhere."
+
+main_file_path="$SCRIPT_DIR/../src/main.rs"
+if [[ ! -f "$main_file_path" ]]; then
+    die "Could not find main file $main_file_path";
+fi
+is_in_main=$(grep -E "const APP_VERSION: &str = \"$app_version\"" "$main_file_path")
+
+echo "✔️  Version is $app_version everywhere!";
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 if ! command -v flatpak; then
     exit 1
@@ -8,13 +8,12 @@ if ! command -v flatpak-builder; then
     exit 1
 fi
 
-# TODO verify also the versions?
+# TODO call check_version.sh
 
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install --user -y flathub org.gnome.Platform/x86_64/3.36 org.gnome.Sdk/x86_64/3.36
 flatpak-builder --force-clean --user build net.louib.panbuild.json
-flatpak-builder --user --run build net.louib.panbuild.json python3 setup.py sdist
-
+flatpak-builder --user --run build net.louib.panbuild.json panbuild -V
 echo "📦 Published the new flatpak package."
 
 # TODO publish on flathub.
