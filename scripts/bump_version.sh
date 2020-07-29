4e8aa78b3439f4a5c38d3b8a9882f6d6d71f38b9
louib <code@louib.net>
2020-07-28 23:54:49 -0400

    Testing for mastah branch?

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -12,6 +12,9 @@ fi
 
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
 branch_name=${branch_name##refs/heads/}
+if [[ ! -z "$branch_name" && "$branch_name" != "master" ]]; then
+    die "You should be on the master branch!";
+fi
 echo "on $branch_name"
 
 # Sanity check.
