2249e7b37a083cb9bc1b2b02c1889a163a077dab
louib <code@louib.net>
2020-07-30 11:39:50 -0400

    🏷️  0.0.6 🏷️

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ extern crate clap;
 use clap::{Arg, App, ArgMatches, SubCommand};
 use std::process::{exit};
 
-const APP_VERSION: &str = "0.0.5";
+const APP_VERSION: &str = "0.0.6";
 
 fn main() {
     let panbuild_app: App = App::new("panbuild")
