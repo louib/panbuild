4f32adbe01d0f485119ea505c3272673cf53533f
louib <code@louib.net>
2020-12-31 00:01:53 -0500

    Templates for reading and writing config

--- src/execution_context.rs
+++ src/execution_context.rs
@@ -1,24 +1,44 @@
-pub const DEFAULT_SOURCE_TYPE: &str = "snap";
-pub const DEFAULT_DESTINATION_TYPE: &str = "flatpak";
+use std::collections::BTreeMap;
+
+use serde::{Deserialize, Serialize};
+
+pub const DEFAULT_SOURCE_TYPE: &str = "flatpak";
 
 pub struct ExecutionContext {
     pub source_filename: String,
     pub source_type: String,
-    pub destination_type: String,
     pub data_dir: String,
     pub content: String,
     pub manifest: crate::manifests::manifest::AbstractManifest,
 }
-
 impl Default for ExecutionContext {
     fn default() -> Self {
         return ExecutionContext {
             source_filename: "".to_string(),
             source_type: DEFAULT_SOURCE_TYPE.to_string(),
-            destination_type: DEFAULT_DESTINATION_TYPE.to_string(),
             data_dir: "".to_string(),
             content: "".to_string(),
             manifest: crate::manifests::manifest::AbstractManifest::default(),
         };
     }
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+#[serde(default)]
+pub struct PanbuildConfig {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub current_workspace: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub last_build: Option<String>,
+
+    pub envs: BTreeMap<String, String>,
+}
+
+pub fn write_config(config: &PanbuildConfig) -> Result<PanbuildConfig, String> {
+    Ok(PanbuildConfig::default())
+}
+
+pub fn read_config() -> Result<PanbuildConfig, String> {
+    Ok(PanbuildConfig::default())
 }
