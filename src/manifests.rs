d23a5785765789ad9633193db26c02b5c699a49e
louib <code@louib.net>
2021-01-02 18:11:18 -0500

    Only the run_build function for now.

--- src/manifests.rs
+++ src/manifests.rs
@@ -90,3 +90,17 @@ pub fn add_module(ctx: &mut crate::execution_context::ExecutionContext, module: 
     eprintln!("Could not get modules for whatever we tried to get modules for.");
     return 0;
 }
+
+pub fn run_build(ctx: &crate::execution_context::ExecutionContext) -> i32 {
+    if let Some(_) = ctx.manifest.flatpak_manifest {
+        match crate::manifests::flatpak::run_build(&ctx) {
+            Ok(r) => return 0,
+            Err(e) => {
+                return 1;
+            },
+        };
+    }
+
+    eprintln!("Could not run build with whatever we tried to run a build with.");
+    return 0;
+}
