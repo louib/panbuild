b4078aa48108836c072878c65cb83f6cd1c49e3e
louib <code@louib.net>
2021-01-11 13:28:03 -0500

    Finally a working script

--- scripts/get_flathub_manifests.py
+++ scripts/get_flathub_manifests.py
@@ -1,16 +1,21 @@
 import sys
+import os
 
 import requests
 
 
 def get_all_flathub_repositories():
+    headers = {}
+    if os.environ.get('GH_TOKEN'):
+        headers['Authorization'] = 'token ' + os.environ.get('GH_TOKEN')
+
     projects_url = "https://api.github.com/orgs/flathub/repos?type=all&per_page=100"
     projects = []
 
     next_page_url = projects_url
     while next_page_url:
         print("Fetching page", next_page_url, file=sys.stderr)
-        response = requests.get(next_page_url)
+        response = requests.get(next_page_url, headers=headers)
 
         try:
             # Making sure there was no error.
@@ -31,12 +36,16 @@ def get_all_flathub_repositories():
             next_page_url = None
             break
 
+        has_next_url = False
         for link in link_header.split(','):
             [url, rel] = link.split(';')
             if rel.strip() == 'rel="next"':
                 # Removing leading < and trailing >
                 next_page_url = url.strip()[1:-1]
+                has_next_url = True
                 break
+        if not has_next_url:
+            break
 
     return projects
 
