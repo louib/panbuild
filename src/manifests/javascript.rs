f5b9391ffa87a4bc42380ca219027a6026345845
louib <code@louib.net>
2021-01-17 12:29:07 -0500

    Parsing function for Javascript package manifest.

--- src/manifests/javascript.rs
+++ src/manifests/javascript.rs
@@ -1,9 +1,11 @@
+use std::collections::BTreeMap;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 #[serde(default)]
-pub struct JavascriptPackage {
+pub struct JavascriptPackageManifest {
     pub name: String,
 
     // FIXME could be translated to a semver.
@@ -14,4 +16,37 @@ pub struct JavascriptPackage {
     pub repository: String,
 
     pub files: Vec<String>,
+
+    pub license: String,
+    pub author: JavascriptPackageAuthor,
+
+    pub dependencies: BTreeMap<String, String>,
+    pub dev_dependencies: BTreeMap<String, String>,
+}
+impl JavascriptPackageManifest {
+    pub fn parse(manifest_content: &String) -> Option<JavascriptPackageManifest> {
+        let js_package_manifest: JavascriptPackageManifest = match serde_json::from_str(&manifest_content) {
+            Ok(m) => m,
+            Err(e) => {
+                log::debug!("Failed to parse the Javascript package manifest: {}.", e);
+                return None;
+            }
+        };
+
+        // TODO I think there's other fields to validate here.
+        if js_package_manifest.name.is_empty() {
+            log::debug!("Required top-level field name is missing from the Javascript package manifest.");
+            return None;
+        }
+
+        Some(js_package_manifest)
+    }
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(default)]
+pub struct JavascriptPackageAuthor {
+    pub name: String,
+    pub email: String,
 }
