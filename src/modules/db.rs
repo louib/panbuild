f8dedd6ef6dec7fb9f915b391bf69b510714eb5c
louib <code@louib.net>
2021-01-25 15:38:47 -0500

    Remove core modules.

--- src/modules/db.rs
+++ src/modules/db.rs
@@ -4,26 +4,19 @@ use std::fs;
 pub const MODULES_DIR: &str = "~/.panbuild/modules/";
 
 pub fn get_all() -> Vec<crate::modules::module::SoftwareModule> {
-    let mut core_modules = self::get_core_modules();
-
     let json_modules_db_path = env::var("PB_MODULES_DB_PATH").unwrap_or(String::from("")).to_string();
     if json_modules_db_path.is_empty() {
-        return core_modules;
+        return vec![];
     }
 
     let json_modules = match fs::read_to_string(&json_modules_db_path) {
         Ok(content) => content,
         Err(e) => {
             eprintln!("could not read file {}.", json_modules_db_path);
-            return core_modules;
+            return vec![];
         }
     };
     let mut db_modules: Vec<crate::modules::module::SoftwareModule> = serde_json::from_str(&json_modules).unwrap();
 
-    core_modules.append(&mut db_modules);
-    core_modules
-}
-
-pub fn get_core_modules() -> Vec<crate::modules::module::SoftwareModule> {
-    vec![]
+    db_modules
 }
