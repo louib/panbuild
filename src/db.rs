127535ab43f4628ecab25a01329139f34124a817
louib <code@louib.net>
2021-01-25 15:25:15 -0500

    Removing hardcoded core projects.

--- src/db.rs
+++ src/db.rs
@@ -2,3 +2,8 @@ pub struct Database {
     pub projects: Vec<crate::projects::project::Project>,
     pub modules: Vec<crate::modules::module::SoftwareModule>,
 }
+impl Database {
+    pub fn get_loaded_database() -> Option<Vec<Database>> {
+        None
+    }
+}
