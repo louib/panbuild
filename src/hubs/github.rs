31959dd735c4b07e82afef7b10f7c35d2239207c
louib <code@louib.net>
2021-02-07 10:29:36 -0500

    Extracting get_next_page_url

--- src/hubs/github.rs
+++ src/hubs/github.rs
@@ -4,6 +4,21 @@ use futures::executor::block_on;
 use reqwest::header;
 use serde::{Deserialize, Serialize};
 
+fn get_next_page_url(link_header: &str) -> &str {
+    for link in link_header.split(",") {
+        let mut link_parts = link.split(";");
+        let url = link_parts.nth(0).unwrap();
+        let rel = link_parts.nth(1).unwrap();
+        if !rel.contains("rel=\"next\"") {
+            continue;
+        }
+        let mut next_page_url = url.trim();
+        next_page_url = &next_page_url[1..next_page_url.len() - 1];
+        return next_page_url;
+    }
+    ""
+}
+
 pub fn get_org_repos(repo_name: &str) -> Vec<String> {
     let mut next_url = format!("https://api.github.com/orgs/{}/repos?type=all&per_page=100", repo_name);
     let mut repos = vec![];
@@ -35,16 +50,7 @@ pub fn get_org_repos(repo_name: &str) -> Vec<String> {
             Some(h) => h.to_str().unwrap(),
             None => return repos,
         };
-        for link in link_header.split(",") {
-            let mut link_parts = link.split(";");
-            let url = link_parts.nth(0).unwrap();
-            let rel = link_parts.nth(1).unwrap();
-            if !rel.contains("rel=\"next\"") {
-                continue;
-            }
-            // next_page_url = url.strip()[1:-1]
-        }
-        next_url = "".to_string();
+        next_url = get_next_page_url(link_header).to_string();
     }
 
     repos
