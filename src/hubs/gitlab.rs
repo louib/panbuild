c4a806ec89a680116cd3b4170a94fcc44282c7b5
louib <code@louib.net>
2021-02-08 19:45:15 -0500

    Starting GitLab hub.

--- src/hubs/gitlab.rs
+++ src/hubs/gitlab.rs
@@ -1,4 +1,64 @@
+use reqwest::header;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GitLab {}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GitLabProject {
+    pub id: String,
+    pub name: String,
+    pub name_with_namespace: String,
+    pub created_at: String,
+    pub last_activity_at: String,
+    pub forks_count: i32,
+    pub star_count: i32,
+    pub description: Option<String>,
+    pub default_branch: String,
+    pub ssh_url_to_repo: String,
+    pub http_url_to_repo: String,
+    pub readme_url: String,
+    pub tag_list: Vec<String>,
+}
+impl GitLabProject {
+    pub fn to_software_project(&self) -> crate::modules::SoftwareModule {
+        return crate::modules::SoftwareModule::default();
+    }
+}
+
+pub fn get_repos(gitlab_domain: &str) -> Vec<String> {
+    let mut next_url = format!("https://{}/api/v4/projects?per_page=100", gitlab_domain);
+    let mut repos = vec![];
+
+    let mut headers = header::HeaderMap::new();
+    let client = reqwest::blocking::Client::builder().default_headers(headers).build().unwrap();
+
+    while (next_url.len() != 0) {
+        // TODO make this really asynchronous with async/await.
+        let mut response = match client.get(&next_url).send() {
+            Ok(r) => r,
+            Err(e) => return repos,
+        };
+
+        if response.status().as_u16() == 204 {
+            return repos;
+        }
+
+        // let response_content = response.text().unwrap();
+        let response_headers = response.headers();
+
+        let link_header = match &response_headers.get("link") {
+            Some(h) => h.to_str().unwrap(),
+            None => return repos,
+        };
+        // next_url = get_next_page_url(link_header).to_string();
+        //
+        let projects: Vec<GitLabProject> = match serde_yaml::from_str(&response.text().unwrap()) {
+            Ok(p) => p,
+            Err(e) => continue,
+        };
+    }
+
+    repos
+}
