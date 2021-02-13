20a64f1edc2aa30a2bdf654ff26bda8ddcf81119
louib <code@louib.net>
2021-02-13 14:46:53 -0500

    Migrate the brew urls.

--- src/hubs/brew.rs
+++ src/hubs/brew.rs
@@ -13,5 +13,13 @@ pub struct HomebrewRecipe {
 }
 
 pub fn get_projects() -> Vec<crate::projects::project::SoftwareProject> {
+    // All the formulae for macOS
+    // wget https://formulae.brew.sh/api/formula.json
+
+    // All the formulae for Linux
+    // wget https://formulae.brew.sh/api/formula-linux.json
+
+    // All the casks
+    // wget https://formulae.brew.sh/api/cask.json
     vec![]
 }
