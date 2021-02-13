cfa1040a8e83f8c139bf25c7be4f2f27a5a820df
louib <code@louib.net>
2021-02-13 14:46:01 -0500

    First brew fetching template

--- src/hubs/brew.rs
+++ src/hubs/brew.rs
@@ -4,5 +4,14 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct HomebrewRecipe {
-    pub id: String,
+    pub name: String,
+    pub desc: String,
+    pub homepage: String,
+
+    // pub urls: String,
+    // pub versions: String,
+}
+
+pub fn get_projects() -> Vec<crate::projects::project::SoftwareProject> {
+    vec![]
 }
