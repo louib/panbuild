f48e154705774e28b3b17e31ba59d2a6f1ea216e
louib <code@louib.net>
2021-02-14 15:53:30 -0500

    Pruning brew parsing from scripts.

--- scripts/get_projects.sh
+++ scripts/get_projects.sh
@@ -23,9 +23,5 @@ PB_OUT_DIR="$tmp_dir" bash "$SCRIPT_DIR/get_debian_source_packages.sh"
 # shellcheck disable=SC2002
 cat "$tmp_dir/sources.txt" | python3 "$SCRIPT_DIR/parse_debian_source_packages.py" > "$tmp_dir/projects/debian_projects.json"
 
-PB_OUT_DIR="$tmp_dir" bash "$SCRIPT_DIR/get_brew_packages.sh"
-# shellcheck disable=SC2002
-cat "$tmp_dir/formula-linux.json" | python3 "$SCRIPT_DIR/parse_brew_packages.py" > "$tmp_dir/projects/brew_projects.json"
-
 PB_OUT_DIR="$tmp_dir/projects/" python3 "$SCRIPT_DIR/merge_projects.py"
 mv "$tmp_dir/projects/all_projects.json" "$PB_PROJECTS_DB_PATH"
